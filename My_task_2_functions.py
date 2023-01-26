cabd1e0a846a91c28ba859d7c24b102e9e75b3f5
Svichkariov <svichkariov@yandex.by>
2023-01-26 12:18:29 +0300

    Исправил задание 2 - функции

--- My_task_2_functions.py
+++ My_task_2_functions.py
@@ -1,9 +1,6 @@
 # Реализовать функцию, которая принимает форму и возвращает ее в обратном порядке.
 def rev (value):
-    value.reverse()
-    return value
-a = [1,2,3,4,5,6]
-print(rev(a))
+    return value[::-1]
 
 # Реализовать функцию, которая принимает два параметра: число и степень - и возвращает это число,
 # сложное в степени.
@@ -14,8 +11,12 @@ def square (number, degree=2):
 # Реализовать функцию, которая принимает стандартный набор значений и возвращает кортеж,
 # Типы передаваемых параметров.
 
-def tpl(value: tuple):
-    return value
+def tpl(*args):
+    a = []
+    for i in args:
+        a.append(type(i))
+    result = tuple(a)
+    return result
 
 # Реализовать функцию, которая принимает произвольный набор именных параметров и возвращает их
 # группировка параметров в видео словаря.
@@ -28,29 +29,17 @@ def tpl(value: tuple):
 # дикт: [['е', {1: 2}]]
 # }
 def sort_type(**dict_):
-    answer =dict.fromkeys(['int', 'str', 'float', 'dict'])
-    i_n=[]
-    s=[]
-    f=[]
-    d=[]
-    for i in dict_:
-        if type(dict_[i]) == int:
-            i_n += ([i,dict_[i]],)
-            answer['int'] = i_n
-        elif type(dict_[i]) == str:
-            s += ([i,dict_[i]],)
-            answer['str'] = s
-        elif type(dict_[i]) == float:
-            f += ([i,dict_[i]],)
-            answer['float'] = f
-        elif type(dict_[i]) == dict:
-            d += ([i,dict_[i]],)
-            answer['dict'] = d
-    return answer
+    lst={}
+    for i in dict_.values():
+        lst.setdefault(type(i),[])
 
+    for k in lst:
+        for j in dict_.keys():
+            if k == type(dict_[j]):
+                lst[k].append([j,dict_[j]])
+    return lst
 
 print(sort_type(a=34, b='some text', c=2, d=1.3, e={1: 2}, f=-3.0))
-
 
 
 # Реализовать функцию, которая принимает строку и произвольный набор неименованных и именованных параметров.
@@ -62,26 +51,24 @@ print(sort_type(a=34, b='some text', c=2, d=1.3, e={1: 2}, f=-3.0))
 
 def replacement(str_, *args, **kwargs):
     st = str_.replace('**', '*')
-    one_word = st.split()
-    k = 0
-    for i in one_word:
+    li = list(kwargs)
+    ansver_lst = []
+    for i in st.split():
         if i[0] == i[-1]:
-            symbol = i[1:-1]
-            if symbol == "first":
-                one_word[k] = kwargs['first']
-            elif symbol == "second":
-                one_word[k] = kwargs['second']
-        k += 1
+            if li[0] == i[1:-1]:
+                i = kwargs[f'{li[0]}']
+            elif li[1] == i[1:-1]:
+                i = kwargs[f'{li[1]}']
 
-    l = 0
-    for j in one_word:
-        if type(j) == str:
-            if j[0] == j[-1] and len(j) != 1:
-                index = int(j[1])
-                one_word[l] = args[index]
-        l += 1
+        if type(i) == str and len(i) != 1 and i[0] == '*':
+            index = int(i[1])
+            i = args[index]
 
-    ansver = [str(n) for n in one_word]
-    return " ".join(ansver)
+        ansver_lst.append(i)
 
-print(replacement('Some *first* string ** to be formatted *2* *first* *second* *0* *2*',111, 'string', '!!!', first=5, second=7,))
+    ansver = ' '.join(str(x) for x in ansver_lst)
+    return ansver
+
+
+print(replacement('Some *first* string ** to be formatted *1* *first* *second* *0* *2*', 111, 'string', '!!!', first=5,
+                  second=7, ))
